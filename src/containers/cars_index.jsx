9d5606f151c7ef519e0655c948a1c3d7f5d7bebc
Noemie Brancourt <noemie_brancourt@hotmail.com>
2022-02-01 17:12:17 +0100

    index page styling

--- src/containers/cars_index.jsx
+++ src/containers/cars_index.jsx
@@ -17,9 +17,8 @@ class CarsIndex extends Component {
           <div className="card-product">
             <img src="../../assets/car-icon.png" />
             <div className="card-product-infos">
-              <h2>{car.brand} {car.model}</h2>
-              <p>{car.owner}</p>
-              <p>{car.plate}</p>
+              <h2><strong>{car.brand} {car.model}</strong></h2>
+              <p><strong>Owner: </strong>{car.owner}</p>
             </div>
           </div>
         </Link>
@@ -31,11 +30,11 @@ class CarsIndex extends Component {
     return (
       <div className="row">
         <div className="list-group-item col-xs-3 card-trip">
-          <img src="https://raw.githubusercontent.com/lewagon/fullstack-images/master/uikit/greece.jpg" />
+          <img src="https://images.unsplash.com/photo-1570129476815-ba368ac77013?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" />
           <div className="card-trip-infos">
             <h3>Garage {this.props.garageName}</h3>
             <Link className="btn btn-default" to="/cars/new">Add a new car</Link>
-            <img src="https://kitt.lewagon.com/placeholder/users/krokrob" className="card-trip-user avatar-bordered" />
+            <img src="https://raw.githubusercontent.com/lewagon/fullstack-images/master/uikit/logo.png" className="card-trip-user avatar-bordered" />
           </div>
         </div>
         <div className="list-group-item col-xs-9">
