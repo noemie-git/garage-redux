38578d57e7d0b1d80e7b68b438634e6fa5eaed0d
Noemie Brancourt <noemie_brancourt@hotmail.com>
2022-02-07 20:06:59 +0100

    installed redux-form package

---
+++ src/containers/cars_new.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+class CarsNew extends Component {
+  render() {
+    return (
+      <div className="row garage-container">
+        <div className="list-group-item col-xs-3 card-trip">
+          <img src="https://images.unsplash.com/photo-1570129476815-ba368ac77013?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" alt="" />
+          <div className="card-trip-infos">
+            <p>Our garage is the most reliable and will be happy to advise no matter the type of vehicle.</p>
+            <Link className="btn btn-default" to="/">Back to list </Link>
+            <img src="https://raw.githubusercontent.com/lewagon/fullstack-images/master/uikit/logo.png" alt="" className="card-trip-user avatar-bordered" />
+          </div>
+        </div>
+        <div className="list-group-item col-xs-9">
+          <div className="new-car-form">
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default CarsNew;
--- src/index.jsx
+++ src/index.jsx
@@ -9,6 +9,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { createHistory as history } from 'history';
 import CarsIndex from './containers/cars_index';
 import CarsShow from './containers/cars_show';
+import CarsNew from './containers/cars_new';
 
 import '../assets/stylesheets/application.scss';
 import carsReducer from './reducers/cars_reducer';
@@ -38,7 +39,7 @@ ReactDOM.render(
       <Switch>
         <Route path="/" exact component={CarsIndex} />
         <Route path="/cars/:id" component={CarsShow} />
-        {/* <Route path="/cars/new" component={CarsNew} /> */}
+        <Route path="/cars/new" component={CarsNew} />
       </Switch>
     </Router>
   </Provider>,
