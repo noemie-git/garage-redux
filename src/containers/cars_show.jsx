75f60525b6db79a9f0ed3e5b4e8941ed7cd3b9eb
Noemie Brancourt <noemie_brancourt@hotmail.com>
2022-02-09 17:31:34 +0100

    destroy method fixed

--- src/containers/cars_show.jsx
+++ src/containers/cars_show.jsx
@@ -6,26 +6,27 @@ import { connect } from 'react-redux';
 import { fetchCar, destroyCar, fetchCars } from '../actions';
 
 class CarsShow extends Component {
-  componentDidMount() {
-    if (!this.props.car) {
-      this.props.fetchCar(this.props.match.params.id);
-    }
-  }
-
-  shouldComponentUpdate() {
-    console.log(`shouldComponentUpdate: ${this.props.match.params.id}`);
-    return this.props.match.params.id !== undefined;
-  }
-
-  destroyCar(event) {
-    console.log(`Delete event is ${this.props.match.params.id}`);
-    event.preventDefault();
-    destroyCar(this.props.match.params.id);
-  }
+  handleClick = () => {
+    this.props.destroyCar(this.props.history, this.props.car);
+  };
 
   render() {
     if (!this.props.car) {
-      return <p>Loading...</p>;
+      return (
+        <div className="row garage-container">
+          <div className="list-group-item col-xs-3 card-trip">
+            <img src="https://images.unsplash.com/photo-1570129476815-ba368ac77013?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" alt="" />
+            <div className="card-trip-infos">
+              <p>Our garage is the most reliable and will be happy to advise no matter the type of vehicle.</p>
+              <Link className="btn btn-default" to="/">Back to list </Link>
+              <img src="https://raw.githubusercontent.com/lewagon/fullstack-images/master/uikit/logo.png" alt="" className="card-trip-user avatar-bordered" />
+            </div>
+          </div>
+          <div className="list-group-item col-xs-9">
+            <p>Loading...</p>
+          </div>
+        </div>
+      );
     }
     const numPlate = this.props.car.plate.toUpperCase();
     const brandUpCased = this.props.car.brand.toUpperCase();
@@ -48,7 +49,7 @@ class CarsShow extends Component {
               <h3><strong>{brandUpCased} {modelUpCased}</strong></h3>
               <p><strong>Owner: </strong>{this.props.car.owner}</p>
               <p className="num-plate"><strong>{numPlate}</strong></p>
-              <Link onClick={this.destroyCar} className="btn btn-danger" id="delete" to="\" ><i className="fa fa-trash" aria-hidden="true" />Delete</Link>
+              <Link onClick={this.handleClick} className="btn btn-danger" id="delete" to="\" ><i className="fa fa-trash" aria-hidden="true" />Delete</Link>
             </div>
           </div>
         </div>
