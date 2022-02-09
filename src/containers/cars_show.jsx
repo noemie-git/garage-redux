67d8204f13966ae7ad95e8a259354f06af198d1a
Noemie Brancourt <noemie_brancourt@hotmail.com>
2022-02-09 17:58:45 +0100

    car show page tweaks

--- src/containers/cars_show.jsx
+++ src/containers/cars_show.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
-import { fetchCar, destroyCar, fetchCars } from '../actions';
+import { destroyCar } from '../actions';
 
 class CarsShow extends Component {
   handleClick = () => {
@@ -37,6 +37,7 @@ class CarsShow extends Component {
         <div className="list-group-item col-xs-3 card-trip">
           <img src="https://images.unsplash.com/photo-1570129476815-ba368ac77013?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" alt="" />
           <div className="card-trip-infos">
+            <h3>{this.props.garage}</h3>
             <p>Our garage is the most reliable and will be happy to advise no matter the type of vehicle.</p>
             <Link className="btn btn-default" to="/">Back to list </Link>
             <img src="https://raw.githubusercontent.com/lewagon/fullstack-images/master/uikit/logo.png" alt="" className="card-trip-user avatar-bordered" />
@@ -65,7 +66,7 @@ function mapStateToProps(state, props) {
 }
 
 function mapDispatchToProps(dispatch) {
-  return bindActionCreators({ fetchCar, destroyCar, fetchCars }, dispatch);
+  return bindActionCreators({ destroyCar }, dispatch);
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(CarsShow);
